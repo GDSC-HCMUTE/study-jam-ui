ba4fc9070dec36be70f85be0e47c216d07b09759
TinMH <tinhongmai1012@gmail.com>
2024-09-07 15:15:57 +0700

    Add Table Component

--- app/(overview)/page.tsx
+++ app/(overview)/page.tsx
@@ -1,7 +1,27 @@
+import CustomTable from "@/components/CustomTable/CustomTable";
+const DataTable = [
+  {
+    key: '1',
+    userName: 'Mai Hồng Tín',
+    email: 'tinhongmai@gmail.com',
+    creationDate: '2023-09-05',
+    event: 'Event A',
+    team: 'Team X',
+  },
+  {
+    key: '2',
+    userName: 'Admin',
+    email: 'SuperAdmin@gmail.com',
+    creationDate: '2023-09-02',
+    event: 'Event B',
+    role: 'Leader',
+  },
+];
 const Home = () => {
   return (
     <div>
       <h1 className="text-lg font-bold">Home</h1>
+      <CustomTable data = {DataTable}/>
     </div>
   );
 };
